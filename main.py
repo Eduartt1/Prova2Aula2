1522538c9fc23367df572abc0c44fff337cba533
Eduardo Marcelino <139513735+Eduartt1@users.noreply.github.com>
2024-12-01 14:52:23 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+
+if numero > 0:
+    print(f"Número {numero} é positivo.")
+elif numero < 0:
+    print(f"Número {numero} é negativo.")
+else:
+    print(f"Número {numero}")
